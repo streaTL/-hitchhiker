fca7deff8ada0f2b8f41fe77d556067f3da4a5b4
최영창 <choiyc1446@gmail.com>
2023-05-21 23:42:22 +0900

    main 페이지 여행지 목록 출력

--- enjoytrip/src/views/MainView.vue
+++ enjoytrip/src/views/MainView.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
     <header-component></header-component>
-    <div>기능 추가할 예정</div>
+    <div>기능 추가 작업중</div>
+    <div style="display: flex; justify-content: center">
+      <div
+        class="mb-auto ms-3 me-3 row"
+        style="flex-wrap: wrap; width: 80%; position: relative"
+      >
+        <list-component
+          v-for="(trip, index) in trips"
+          :key="index"
+          :trip="trip"
+        ></list-component>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderComponent from "@/components/common/HeaderComponent.vue";
+import axios from 'axios';
+
+
 export default {
   name: "MainView",
   components: { HeaderComponent },
   data() {
     return {
-      message: "",
+      trips : []
     };
   },
-  created() {},
+  created() {
+    axios.get("http://localhost/map/ramAttrInfo").then(({data}) => trip = data)
+  },
   methods: {},
 };
 </script>
